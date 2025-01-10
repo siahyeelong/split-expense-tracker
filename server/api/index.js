0e18a78477ac010c00d801551947d45015fec5c6
Siah Yee Long <yeelongsiah@gmail.com>
2025-01-10 23:55:49 +0800

    allowed listening only on dev

--- server/api/index.js
+++ server/api/index.js
@@ -3,14 +3,14 @@ import cors from "cors";
 import records from "../routes/record.js";
 import colors from "colors";
 
-const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
 
-// start the Express server
+// start the Express server only if in dev mode
+const PORT = process.env.PORT || 5050;
 if (process.env.NODE_ENV === 'dev') {
     app.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`.green.bold);
