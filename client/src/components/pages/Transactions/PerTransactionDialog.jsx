2e394f95e35017337ec8bdd7bd04e2f31c155345
Siah Yee Long <yeelongsiah@gmail.com>
2025-01-15 22:58:47 +0800

    made a border around categories icon

--- client/src/components/pages/Transactions/PerTransactionDialog.jsx
+++ client/src/components/pages/Transactions/PerTransactionDialog.jsx
@@ -1,16 +1,29 @@
-import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
+import { Dialog, DialogActions, DialogContent, DialogTitle, Box, Typography } from '@mui/material'
 import React from 'react'
 import { Categories } from '../../settings/Categories';
 
 function TransactionCard(transaction) {
     const trans = transaction.transaction
+    const price = parseFloat(trans.price).toLocaleString('en-SG', {
+        style: 'currency', currency: (trans.currency === 'SGD') ? 'SGD' : (trans.currency || 'SGD'),
+        minimumFractionDigits: 0,  // Show no decimal places if not needed
+        maximumFractionDigits: 2
+    })
+
     return (
         <>
-            {/* Display price large */}
-            {/* Display category icon in the same line */}
-            {Categories.getCategoryIcon(trans.category)}
+            <Box display={'flex'} justifyContent={'space-between'} alignContent={'center'}>
+                {/* Display category icon in the same line */}
+                {Categories.getCategoryIcon(trans.category)}
+                {/* Display price large */}
+                <Typography fontSize={'large'}>{price}</Typography>
+            </Box>
 
             {/* Display Category in text */}
+            <Box display={'flex'} justifyContent={'space-between'} alignContent={'center'}>
+                <Typography fontWeight={'bold'}>Category: </Typography>
+                <Typography>{trans.category}</Typography>
+            </Box>
             {/* Display Description */}
             {/* Display amount in SGD if not in original currency */}
             {/* Display exchange rate */}
@@ -24,7 +37,7 @@ function TransactionCard(transaction) {
 
 function PerTransactionDialog({ showDialog, transaction, onClose }) {
     return (
-        <Dialog open={showDialog} onClose={onClose}>
+        <Dialog open={showDialog} onClose={onClose} fullWidth={true} maxWidth={'xs'}>
             <DialogTitle>Transaction number: {transaction.id}</DialogTitle>
             <DialogContent><TransactionCard transaction={transaction} /></DialogContent>
             <DialogActions></DialogActions>
--- client/src/components/settings/Categories.js
+++ client/src/components/settings/Categories.js
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import RestaurantRoundedIcon from '@mui/icons-material/RestaurantRounded';
 import CommuteRoundedIcon from '@mui/icons-material/CommuteRounded';
 import LocalMallRoundedIcon from '@mui/icons-material/LocalMallRounded';
@@ -11,20 +12,39 @@ class CategoriesClass {
     constructor() {
         this.category = {}
     }
-    setCategoryIcon(cat, icon) {
-        this.category[cat] = { icon: icon };
+    // setCategoryIcon(cat, icon) {
+    //     this.category[cat] = { icon: icon };
+    // }
+    setCategoryIcon(cat, icon, colour) {
+        this.category[cat] = { icon: <BackgroundBox colour={colour} icon={icon} /> };
     }
     getCategoryIcon(cat) {
         return this.category[cat] ? this.category[cat].icon : "no icon";
     }
 }
 
+const BackgroundBox = ({ colour, icon }) => {
+    return (
+        <Box sx={{
+            width: 40, // Adjust size as needed
+            height: 40,
+            borderRadius: '50%',
+            backgroundColor: colour, // Background color of the circle
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            {icon}
+        </Box>
+    )
+}
+
 export const Categories = new CategoriesClass()
-Categories.setCategoryIcon('Food', <RestaurantRoundedIcon />)
-Categories.setCategoryIcon('Transport', <CommuteRoundedIcon />)
-Categories.setCategoryIcon('Shopping', <LocalMallRoundedIcon />)
-Categories.setCategoryIcon('Entertainment', <AttractionsRoundedIcon />)
-Categories.setCategoryIcon('Accomodation', <KingBedRoundedIcon />)
-Categories.setCategoryIcon('Others', <SellRoundedIcon />)
-Categories.setCategoryIcon('Insurance', <HealthAndSafetyRoundedIcon />)
-Categories.setCategoryIcon('Ferry', <DirectionsBoatFilledRoundedIcon />)
+Categories.setCategoryIcon('Food', <RestaurantRoundedIcon style={{ color: '#f8f9fa' }} />, '#ee9b00')
+Categories.setCategoryIcon('Transport', <CommuteRoundedIcon style={{ color: '#f8f9fa' }} />, '#0a9396')
+Categories.setCategoryIcon('Shopping', <LocalMallRoundedIcon style={{ color: '#f8f9fa' }} />, '#ae2012')
+Categories.setCategoryIcon('Entertainment', <AttractionsRoundedIcon style={{ color: '#f8f9fa' }} />, '#ca6702')
+Categories.setCategoryIcon('Accomodation', <KingBedRoundedIcon style={{ color: '#f8f9fa' }} />, '#52796f')
+Categories.setCategoryIcon('Others', <SellRoundedIcon style={{ color: '#f8f9fa' }} />, '#6b705c')
+Categories.setCategoryIcon('Insurance', <HealthAndSafetyRoundedIcon style={{ color: '#f8f9fa' }} />, '#f26a8d')
+Categories.setCategoryIcon('Ferry', <DirectionsBoatFilledRoundedIcon style={{ color: '#f8f9fa' }} />, '#94d2bd')
